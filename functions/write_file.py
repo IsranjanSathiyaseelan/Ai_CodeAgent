b90c29840f3381efaae71f8059e60cd2a755949c
Isranjan <isranjansathiyaseelan@gmail.com>
2026-03-28 17:52:45 +0530

    updated write file

--- functions/write_file.py
+++ functions/write_file.py
@@ -5,7 +5,16 @@ def write_file(working_directory, file_path, content):
     abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
     if not abs_file_path.startswith(abs_working_dir):
         return f'Error: "{file_path}" is not in the working directory'
-    if not os.path.isfile(abs_file_path):
-        parent_dir = os.path.dirname(abs_file_path)
-        os.makedirs(parent_dir)
     
+    parent_dir = os.path.dirname(abs_file_path)
+    if not os.path.isdir(parent_dir):
+        try:
+            os.makedirs(parent_dir)
+        except Exception as e:
+            return f"Could not create parent dirs: {parent_dir} = {e}"
+    try:
+        with open(abs_file_path, 'w') as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Failed to write to file: {file_path}, {e}"
--- test.py
+++ test.py
@@ -1,33 +1,11 @@
-from functions.get_files_info import get_files_info
-from functions.get_file_content import get_file_content
+# from Ai_CodeAgent.functions.write_file import write_file
+# from functions.get_files_info import get_files_info
+from functions.write_file import write_file
 
 def main():
     working_dir = "calculator"
-    
-    # Print the content of main.py (this file exists)
-    print(get_file_content(working_dir, "main.py"))
-
-    # Print the content of calculator.py (this file exists in pkg folder)
-    print(get_file_content(working_dir, "pkg/calculator.py"))
-
-    # Test case: file does NOT exist
-    # Should return an error: "pkg/notexists.py" is not a file
-    print(get_file_content(working_dir, "pkg/notexists.py"))
-
-    # Test case: file path is outside the working directory
-    # Should return an error: "/bin/cat" is not in the working directory
-    print(get_file_content(working_dir, "/bin/cat"))
-
-    
-
-    # root_contents = get_files_info(working_dir, ".")
-    # print(root_contents)
-    # pkg_contents = get_files_info(working_dir, "pkg")
-    # print(pkg_contents)
-    # pkg_contents = get_files_info(working_dir, "/bin")
-    # print(pkg_contents)
-    # pkg_contents = get_files_info(working_dir, "../")
-    # print(pkg_contents)
-    
+    print(write_file(working_dir, "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(write_file(working_dir, "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print(write_file(working_dir, "pkg2/temp.txt", "this should  be allowed"))
 
 main()
