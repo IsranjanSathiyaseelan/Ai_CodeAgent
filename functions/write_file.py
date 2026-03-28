b182e6bce80a8bec3e838f4a204bfcb08e6bc720
Isranjan <isranjansathiyaseelan@gmail.com>
2026-03-28 16:49:30 +0530

    added write file

---
+++ functions/write_file.py
@@ -0,0 +1,11 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: "{file_path}" is not in the working directory'
+    if not os.path.isfile(abs_file_path):
+        parent_dir = os.path.dirname(abs_file_path)
+        os.makedirs(parent_dir)
+    
--- test.py
+++ test.py
@@ -1,14 +1,33 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 def main():
     working_dir = "calculator"
-    root_contents = get_files_info(working_dir, ".")
-    print(root_contents)
-    pkg_contents = get_files_info(working_dir, "pkg")
-    print(pkg_contents)
-    pkg_contents = get_files_info(working_dir, "/bin")
-    print(pkg_contents)
-    pkg_contents = get_files_info(working_dir, "../")
-    print(pkg_contents)
+    
+    # Print the content of main.py (this file exists)
+    print(get_file_content(working_dir, "main.py"))
+
+    # Print the content of calculator.py (this file exists in pkg folder)
+    print(get_file_content(working_dir, "pkg/calculator.py"))
+
+    # Test case: file does NOT exist
+    # Should return an error: "pkg/notexists.py" is not a file
+    print(get_file_content(working_dir, "pkg/notexists.py"))
+
+    # Test case: file path is outside the working directory
+    # Should return an error: "/bin/cat" is not in the working directory
+    print(get_file_content(working_dir, "/bin/cat"))
+
+    
+
+    # root_contents = get_files_info(working_dir, ".")
+    # print(root_contents)
+    # pkg_contents = get_files_info(working_dir, "pkg")
+    # print(pkg_contents)
+    # pkg_contents = get_files_info(working_dir, "/bin")
+    # print(pkg_contents)
+    # pkg_contents = get_files_info(working_dir, "../")
+    # print(pkg_contents)
+    
 
 main()
